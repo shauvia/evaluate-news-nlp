0898d9278fcfd56321d77afd5324daf8151a6a77
shauvia <e.szalwinska@gmail.com>
2021-11-03 12:46:59 -0700

    some html created and style css and not needed files removed

--- src/server/index_serv.js
+++ src/server/index_serv.js
@@ -7,3 +7,12 @@ app.use(bodyParser.urlencoded( {extended: false} ));
 app.use(bodyParser.json());
 
 app.use(cors());
+
+const port = 3000;
+
+const server = app.listen(port, listening);
+
+function listening(){
+  console.log('server runnning');
+  console.log(`runnning on localhost ${port}`);
+}
