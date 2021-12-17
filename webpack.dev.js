95705580232ab6513c53544ca0792583dbe2ab1a
shauvia <e.szalwinska@gmail.com>
2021-12-17 13:58:32 -0800

    added source-map-loader and created separate dev mode without webpack dev mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
@@ -12,13 +13,27 @@ module.exports = {
         test: '/\.js$/',
         exclude: /node_modules/,
         loader: 'babel-loader'
-      }
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
     ]
   },
   plugins:[
-    new HtmlWebpackPlugin({
+    new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename:"index.html",
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
     })
   ]
 };
