67f66f17d652ce5eda646bba2bcf5879312973fe
shauvia <e.szalwinska@gmail.com>
2021-12-21 14:09:49 -0800

    production environment up and running, MiniCssExtractPlugin OptimizeCSSAssetsPlugin TerserPlugin installed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,34 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: './src/client/c_index.js',
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   module:{
     rules: [
       {
-        test: '/\.js$/',
+        test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ]
   },
   plugins:[
-    new HtmlWebpackPlugin({
+    new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename:"index.html",
-    })
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" })
   ]
 };
